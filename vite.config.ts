efb2eb37f10ce0ea312232d714398f3e9271f087
Rojano23 <rojanoland23@gmail.com>
2026-08-09 11:39:20 -0600

    First client preview2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: '/DEV-WEBPAGES-RX23/',
+  base: '/EPC DEL SURESTE/',
 
   server: {
     host: '0.0.0.0',
